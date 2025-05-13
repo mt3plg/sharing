198658ac879ec0916500958b2be96e6dcddb16fc
Vlados <vladtzm@gmail.com>
2025-05-13 17:12:02 +0300

    src/prisma.service.ts

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,16 +1,23 @@
+// src/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        super({
+            log: ['query', 'info', 'warn', 'error'],
+        });
+        console.log('Prisma Client initialized with models:', Object.keys(this));
+    }
 
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
+    async onModuleInit() {
+        await this.$connect();
+        console.log('Prisma connected to database');
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+        console.log('Prisma disconnected from database');
+    }
 }
