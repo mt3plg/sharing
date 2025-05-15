ec92fd3fa9b2dd9902a9e2c1d4896c18f6148892
Vlados <vladtzm@gmail.com>
2025-05-15 13:39:36 +0300

    UPD Ride2

--- src/rides/rides.module.ts
+++ src/rides/rides.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { RidesController } from './rides.controller';
 import { RidesService } from './rides.service';
 import { PrismaService } from '../prisma.service';
+import { EmailModule } from '../email/email.module';
 
 @Module({
+  imports: [EmailModule], // Додаємо імпорт EmailModule
   controllers: [RidesController],
   providers: [RidesService, PrismaService],
 })
